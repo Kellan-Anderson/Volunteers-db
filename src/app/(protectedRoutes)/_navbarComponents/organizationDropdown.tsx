dba0778ee58c743942e8614cb9af116605e280e1
Kellan Anderson <kellandanderson@gmail.com>
2024-02-20 20:25:57 -0500

    Added add organization option to organization dropdown

--- src/app/(protectedRoutes)/_navbarComponents/organizationDropdown.tsx
+++ src/app/(protectedRoutes)/_navbarComponents/organizationDropdown.tsx
@@ -2,7 +2,7 @@
 
 import { Building } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "~/components/ui/select";
 import { api } from "~/trpc/react";
 
 type OrganizationDropdownProps = {
@@ -15,20 +15,29 @@ type OrganizationDropdownProps = {
 
 export function OrganizationDropdown({ organizationsList, usersLastOrganization } : OrganizationDropdownProps) {
 	const router = useRouter();
-	const { mutate } = api.users.updateLastOrganization.useMutation({
+	const { mutate: updateOrganization } = api.users.updateLastOrganization.useMutation({
 		onSuccess: () => router.refresh()
 	});
+
 	return (
 		<div className="flex flex-row gap-1">
 			<Building className="h-9 w-9"/>
-			<Select defaultValue={usersLastOrganization} onValueChange={(selection) => mutate({ orgId: selection })}>
+			<Select defaultValue={usersLastOrganization} onValueChange={(selection) => updateOrganization({ orgId: selection })}>
 				<SelectTrigger>
 					<SelectValue placeholder="Choose an organization"/>
 				</SelectTrigger>
 				<SelectContent>
-					{organizationsList.map(org => (
-						<SelectItem value={org.id} key={org.id}>{org.name}</SelectItem>
-					))}
+					<SelectGroup>
+						{organizationsList.map(org => (
+							<SelectItem value={org.id} key={org.id}>{org.name}</SelectItem>
+						))}
+						<SelectLabel
+							className="hover:bg-accent font-bold cursor-pointer"
+							onClick={() => router.push('/new-organization')}
+						>
+							Create a new organization
+						</SelectLabel>
+					</SelectGroup>
 				</SelectContent>
 			</Select>
 		</div>
