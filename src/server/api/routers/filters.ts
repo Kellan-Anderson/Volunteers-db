c38bf294593710344c9fff5188a8620a57a230d9
Kellan Anderson <kellandanderson@gmail.com>
2024-04-16 23:33:40 -0400

    Added procedures for filter CRUD operations

--- src/server/api/routers/filters.ts
+++ src/server/api/routers/filters.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { and, eq } from "drizzle-orm";
-import { filters } from "~/server/db/schema";
+import { filters, organizationsAndUsers } from "~/server/db/schema";
 import { randomId } from "~/lib/randomId";
 
 export const filterRouter = createTRPCRouter({
@@ -73,5 +73,99 @@ export const filterRouter = createTRPCRouter({
 
 			const allFilters = await ctx.db.query.filters.findMany({ where: eq(filters.organizationId, lastOrganizationId)})
 			return allFilters
-		})
+		}),
+
+	getAllFiltersWithDetails: protectedProcedure
+		.query(async ({ ctx }) => {
+			const { lastOrganizationId } = ctx.session.user;
+			if(!lastOrganizationId) throw new Error('User has not setup/joined an organization');
+
+			const organizationFilters = await ctx.db.query.filters.findMany({
+				where: eq(filters.organizationId, lastOrganizationId),
+				with: {
+					volunteers: {
+						with: {
+							volunteer: true
+						}
+					}
+				}
+			});
+
+			const cleanedFilters = organizationFilters.map(f => ({
+				name: f.name,
+				type: f.filterType,
+				numVolunteers: f.volunteers.length
+			}));
+
+			return cleanedFilters
+		}),
+
+	getSingleFilterDetails: protectedProcedure
+		.input(z.object({ filterId: z.string() }))
+		.query(async ({ ctx, input }) => {
+			const { lastOrganizationId } = ctx.session.user;
+			if(!lastOrganizationId) throw new Error('User has not setup/joined an organization');
+
+			const targetFilter = await ctx.db.query.filters.findFirst({
+				where: and(
+					eq(filters.organizationId, lastOrganizationId),
+					eq(filters.id, input.filterId)
+				),
+				with: {
+					volunteers: { with: { volunteer: true } }
+				}
+			});
+
+			if(!targetFilter) return undefined
+
+			return {
+				name: targetFilter.name,
+				type: targetFilter.filterType,
+				volunteers: targetFilter.volunteers.map(v => ({ name: v.volunteer.name }))
+			}
+		}),
+
+	editFilterName: protectedProcedure
+		.input(z.object({ filterId: z.string(), newName: z.string() }))
+		.mutation(async ({ ctx, input }) => {
+			const { lastOrganizationId } = ctx.session.user;
+			if(!lastOrganizationId) throw new Error('User has not setup/joined an organization');
+
+			const filterVerification = await ctx.db.query.filters.findFirst({ where: eq(filters.id, input.filterId) })
+			if(filterVerification?.filterType === 'tag') {
+				throw new Error('Tags can only be edited, not deleted')
+			}
+
+			await ctx.db
+				.update(filters)
+				.set({ name: input.newName })
+				.where(
+					and(
+						eq(filters.organizationId, lastOrganizationId),
+						eq(filters.id, input.filterId)
+					)
+				)
+		}),
+
+		deleteFilter: protectedProcedure
+			.input(z.object({filterId: z.string()}))
+			.mutation(async ({ ctx, input }) => {
+				const { lastOrganizationId } = ctx.session.user;
+				if(!lastOrganizationId) throw new Error('User has not setup/joined an organization');
+
+				const userVerification = await ctx.db.query.organizationsAndUsers.findFirst({
+					where: and(
+						eq(organizationsAndUsers.organizationId, lastOrganizationId),
+						eq(organizationsAndUsers.userId, ctx.session.user.id)
+					)
+				});
+				if(!userVerification || userVerification.permission !== 'admin') {
+					throw new Error('You do not have permission to delete this filter')
+				}
+
+				await ctx.db.delete(filters).where(and(
+					eq(filters.organizationId, lastOrganizationId),
+					eq(filters.id, input.filterId)
+				));
+			})
 })
